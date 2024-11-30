6b3bbaef5d5fdeb8b5ab045ca9d7ddceed249492
Marco Llapapasca <marcollapapasca@gmail.com>
2024-11-30 10:30:46 -0500

    Marco Llapapasca

--- database/save_email_v2.py
+++ database/save_email_v2.py
@@ -81,13 +81,14 @@ class EmailService:
             connection = self.database.get_connection()
             with connection.cursor() as cursor:
                 cursor.execute("""
-                   SELECT users.email as email_user, email_id, subject, body_text, body_html, sender_email, received_at, sent_at, status, sent_status, read_status FROM gmail.emails INNER JOIN gmail.users ON emails.user_id = users.user_id
-                  WHERE 
-                       sent_status = %s
-                        AND LOWER(users.email) NOT LIKE 'postmaster@%' 
-                        AND LOWER(users.email) NOT LIKE 'mailer-daemon@%' 
-                        AND LOWER(users.email) NOT LIKE 'no-reply@%' 
-                        AND LOWER(users.email) NOT LIKE 'sc-noreply@%'
+                  SELECT users.email as email_user, email_id, subject, body_text, body_html, sender_email, received_at, sent_at, status, sent_status, read_status FROM gmail.emails INNER JOIN gmail.users ON emails.user_id = users.user_id
+                    WHERE (
+                        LOWER(users.email) NOT LIKE 'postmaster@%' 
+                        OR LOWER(users.email) NOT LIKE 'mailer-daemon@%' 
+                        OR LOWER(users.email) NOT LIKE 'no-reply@%' 
+                        OR LOWER(users.email) NOT LIKE 'sc-noreply@%'
+                            ) 
+                        and sent_status = %s
                     ORDER BY received_at DESC, sent_at DESC
                     LIMIT 100;
                 """, (sent_status,))
