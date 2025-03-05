1f84da643c7b1ba08349f3dc928f20f87510c945
cu4tro-dgb <saekaronw@gmail.com>
2025-03-05 14:56:40 -0500

    add

--- signature.py
+++ signature.py
@@ -3,10 +3,10 @@ signatureGlobal = """
   <tr>
     <td style="padding-right:10px; border-right: 1px solid #000"">
       <strong>
-        <a href="https://drago.pe" style="text-decoration: none; color: black; font-size:32px;" >DRAGO</a>
+        <a href="https://drago.pe" style="text-decoration: none; color: black; font-size:24px;" >DRAGO</a>
       </strong>
     </td>
-    <td style="padding-left:10px;">
+    <td style="padding-left:10px; font-size:14px;">
       <strong >
         CORPORACIÓN GRUPO DRAGO S.A.C <br>
         RUC: 20608386387<br>
@@ -23,15 +23,15 @@ signatureOther = """
   <tr>
     <td style="padding-right:10px; border-right: 1px solid #000"">
       <strong>
-        <a href="https://drago.pe" style="text-decoration: none; color: black; font-size:32px;" >DRAGO</a>
+        <a href="https://drago.pe" style="text-decoration: none; color: black; font-size:24px;" >DRAGO</a>
       </strong>
     </td>
-    <td style="padding-left:10px;">
+    <td style="padding-left:10px; font-size:14px;">
       <strong >
         CORPORACIÓN GRUPO DRAGO S.A.C <br>
         RUC: 20608386387<br>
+        <a class="flex" href="mailto:contacto@drago.pe">contacto@drago.pe</a><br>
       </strong>
-      <a class="flex" href="mailto:contacto@drago.pe">contacto@drago.pe</a><br>
     </td>
   </tr>
 </table>
